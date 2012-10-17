aa27d384fe0b3985abf3dcc51d053ddceea9151f
lpelypenko <lpelypenko@ebay.com>
2012-10-17 20:05:59 +0300

    Added User Model

---
+++ module/Ekibana/src/Ekibana/Model/User.php
@@ -0,0 +1,17 @@
+<?php
+// module/Ekibana/src/Ekibana/Model/User.php:
+namespace Ekibana\Model;
+
+class User
+{
+    public $id;
+    public $login;
+    public $password;
+
+    public function exchangeArray($data)
+    {
+        $this->id     = (isset($data['id'])) ? $data['id'] : null;
+        $this->login = (isset($data['login'])) ? $data['login'] : null;
+        $this->password  = (isset($data['password'])) ? $data['password'] : null;
+    }
+}
